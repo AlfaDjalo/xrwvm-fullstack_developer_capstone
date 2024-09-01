77975d5feddc36306d60f3bdbc31e458314ffc15
AlfaDjalo <david_a_collier@hotmail.com>
2024-09-01 07:00:03 -0400

    Adding dynamic pages.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,8 +98,8 @@ def registration(request):
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
-        # endpoint = "/fetchDealers"
-        endpoint = "/get_dealers"
+        endpoint = "/fetchDealers"
+        # endpoint = "/get_dealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
